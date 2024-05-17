09007bdcdde04415c1390edc666a17ee205bb0f3
jalarconba <148835203+jalarconba@users.noreply.github.com>
2024-05-16 23:03:26 -0400

    first commit

---
+++ Eva3integracion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3integracion;
+
+/**
+ *
+ * @author Hp Liceo de Minería
+ */
+public class Eva3integracion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
